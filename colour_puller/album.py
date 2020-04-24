c1cce1dd8179a7ee9202cd504c2325f88b14ca06
Adam <adam.ruszkowski@gmail.com>
2020-04-24 11:53:10 +0100

    Add initial AlbumArtwork class

--- colour_puller/album.py
+++ colour_puller/album.py
@@ -1,3 +1,10 @@
+from io import BytesIO
+import os
+import requests
+
+from PIL import Image
+
+
 class SpotifyAlbum:
     def __init__(self, resp_dict, from_api=True):
         self.name = resp_dict.get('name', None)
@@ -41,3 +48,30 @@ class SpotifyAlbum:
     
     def to_dict(self):
         return self.__dict__
+
+
+class AlbumArtwork:
+    original_folder = 'artwork_downloads'
+    edited_folder = 'artwork_edited'
+    palette_folder = 'artwork_palette'
+    for f in (original_folder, edited_folder, palette_folder):
+        if not os.path.isdir(f):
+            os.mkdir(f)
+
+    def __init__(self, source, source_type='url', name=None):
+        if source_type.lower() == 'file':
+            self._original_image = Image.open(source)
+            self.path = source
+        elif source_type.lower() == 'url':
+            resp = requests.get(source)
+            if name is None:
+                new_name = source.split('/')[-1] + '.jpeg'
+            else:
+                new_name = name + '.jpeg'
+            self.path = os.path.join(__class__.original_folder, new_name)
+            with open(self.path, 'wb') as disk_file:
+                disk_file.write(resp.content)
+            
+            self._original_image = Image.open(BytesIO(resp.content))
+
+
